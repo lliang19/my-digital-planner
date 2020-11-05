decb00f4b7937644ecc247b326510e1e54855c2f
Lee Liang <ltliang1@gmail.com>
2020-11-04 22:57:30 -0800

    remove unused imports in config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,6 @@
 /* eslint-disable */
 
 const { override, addLessLoader } = require('customize-cra');
-const reactAppRewirePostcss = require('react-app-rewire-postcss');
-const postcssNormalize = require('postcss-normalize');
 
 module.exports = override(
   addLessLoader({
